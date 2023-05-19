39d6147a09d4c97c439e61c523a155fc4940b28a
anighosal <121676026+anighosal@users.noreply.github.com>
2023-05-19 21:52:19 +0600

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -31,6 +31,39 @@ async function run() {
       const cursor = productCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    });
+    app.get("/mytoys", async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { _id: 0, title: 1, imdb: 1 },
+      };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/toydetails", async (req, res) => {
+      const toydetails = req.body;
+      console.log(toydetails);
     });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
