1fe738c88b16911fe4d50c3ae9d900a6e12fef8f
anighosal <aasgfamily@gmail.com>
2023-12-26 02:00:41 +0600

    myToys name added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-//middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -22,11 +22,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
-
     const productCollection = client.db("animalToys").collection("products");
-    const toysCollection = client.db("animalToys").collection("mytoys");
+    const toysCollection = client.db("animalToys").collection("myToys");
 
     app.get("/products", async (req, res) => {
       const cursor = productCollection.find();
@@ -34,30 +33,24 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/products/:id", async (req, res) => {
+    app.get("/product/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await productCollection.findOne(query);
       res.send(result);
     });
-    // app.get("/products/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await productCollection.findOne(query);
-    //   res.send(result);
-    // });
-    app.get("/products/:id", async (req, res) => {
+
+    app.get("/product/:id/details", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const options = {
-        // Include only the `title` and `imdb` fields in the returned document
         projection: { _id: 0, title: 1, price: 1, img: 1 },
       };
       const result = await productCollection.findOne(query, options);
       res.send(result);
     });
-    //  mytoys
-    app.get("/mytoys", async (req, res) => {
+
+    app.get("/myToys", async (req, res) => {
       console.log(req.query.email);
       let query = {};
       if (req.query?.email) {
@@ -67,15 +60,15 @@ async function run() {
       res.send(result);
     });
 
-    app.post("/mytoys", async (req, res) => {
+    app.post("/myToys", async (req, res) => {
       const orders = req.body;
       console.log(orders);
       const result = await toysCollection.insertOne(orders);
       res.send(result);
     });
-    // update
 
-    app.patch("/mytoys/:id", async (req, res) => {
+    // Update
+    app.patch("/myToys/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedBooking = req.body;
@@ -89,12 +82,13 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/mytoys/:id", async (req, res) => {
+    app.delete("/myToys/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await toysCollection.deleteOne(query);
       res.send(result);
     });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -105,10 +99,11 @@ async function run() {
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("kids gaellery is running");
+  res.send("kids gallery is running");
 });
 
 app.listen(port, () => {
